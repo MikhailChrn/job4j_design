b36a65026b884fb83d64d4ef5e1ca7b65a0ab2c6
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-06-05 08:26:49 +0400

    1. Что такое итератор. [#4951] Реализация итератора для массива. Итератор должен отдавать элементы в обратном порядке.

---
+++ src/main/java/ru/job4j/iterator/BackwardArrayIt.java
@@ -0,0 +1,33 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+//1. Реализуйте итератор для массива.
+// Итератор должен отдавать элементы в обратном порядке.
+
+public class BackwardArrayIt implements Iterator<Integer> {
+    private final int[] data;
+    private int point = 0;
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+        this.point = this.data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (point >= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
