220c71cf27fddb53f7429281fae7d43f6a64685f
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-06-06 08:28:23 +0400

    2. Итератор для двухмерного массива int[][] [#9539  [#9539 #421011]]

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -7,38 +7,18 @@ public class MatrixIt implements Iterator<Integer> {
     private final int[][] data;
     private int row = 0;
     private int column = 0;
-    private int downCounter = 0;
 
     public MatrixIt(int[][] data) {
         this.data = data;
-        this.downCounter = getNumberOfAllValidElements();
-        if (this.downCounter > 0) {
-            this.row = getNumberOfFirstValidRow();
-        }
-    }
-
-    private int getNumberOfAllValidElements() {
-        int count = 0;
-        for (int[] lcRow : data) {
-            count += lcRow.length;
-        }
-        return count;
-    }
-
-    private int getNumberOfFirstValidRow() {
-        int number = 0;
-        for (int lcRow = 0; lcRow < data.length; lcRow++) {
-            if (data[lcRow].length > 0) {
-                number = lcRow;
-                break;
-            }
-        }
-        return number;
     }
 
     @Override
     public boolean hasNext() {
-        return downCounter > 0;
+        while (row < data.length && column >= data[row].length) {
+            column = 0;
+            row++;
+        }
+        return row < data.length && column < data[row].length;
     }
 
     @Override
@@ -46,13 +26,6 @@ public class MatrixIt implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        int result = data[row][column];
-        downCounter--;
-        column++;
-        while (row < data.length && column >= data[row].length) {
-            column = 0;
-            row++;
-        }
-        return result;
+        return data[row][column++];
     }
 }
