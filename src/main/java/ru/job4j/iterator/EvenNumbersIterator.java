bf511a4f4abb3ee13fe9496e03bd468dee2cf81f
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-06-07 08:47:49 +0400

    3. Итератор четных чисел [#150  [#150]]

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -4,46 +4,29 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class EvenNumbersIterator implements Iterator<Integer> {
-
     private int[] data;
-    private int index = -1;
-    private int downCounter;
+    private int index = 0;
 
     public EvenNumbersIterator(int[] data) {
         this.data = data;
-        this.downCounter = getAmountOfEvenNumber();
-        if (this.downCounter > 0) {
-            this.index = getIndexOfEvenNumber(index);
-        }
-    }
-
-    private boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-
-    private int getAmountOfEvenNumber() {
-        int count = 0;
-        for (int i : data) {
-            if (isEven(i)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private Integer getIndexOfEvenNumber(int index) {
-        for (int i = index + 1; i < data.length; i++) {
-            if (isEven(data[i])) {
-                index = i;
-                break;
-            }
-        }
-        return index;
     }
 
     @Override
     public boolean hasNext() {
-        return downCounter > 0;
+        if (index >= data.length || data.length == 0) {
+            return false;
+        }
+        if (data[index] % 2 != 0) {
+            int i = index;
+            while (i < data.length - 1) {
+                i++;
+                if (data[i] % 2 == 0) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        return data[index] % 2 == 0;
     }
 
     @Override
@@ -51,9 +34,6 @@ public class EvenNumbersIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        int output = index;
-        downCounter--;
-        index = getIndexOfEvenNumber(index);
-        return data[output];
+        return data[index++];
     }
 }
