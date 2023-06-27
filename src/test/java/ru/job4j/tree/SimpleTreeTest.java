c35cde739892ac93b0b0ec2525caaed7500f127c
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-06-27 18:30:24 +0400

    1. Создать элементарную структуру дерева [#1711  [#1711]]

---
+++ src/test/java/ru/job4j/tree/SimpleTreeTest.java
@@ -0,0 +1,64 @@
+package ru.job4j.tree;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SimpleTreeTest {
+    @Test
+    void when6ElFindLastThen6() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.findBy(6)).isPresent();
+    }
+
+    @Test
+    void whenElFindNotExistThenOptionEmpty() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        assertThat(tree.findBy(7)).isEmpty();
+    }
+
+    @Test
+    void whenChildExistOnLeafThenNotAdd() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        assertThat(tree.add(2, 6)).isFalse();
+    }
+
+    @Test
+    void whenAddingSevenElementsConsistently() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(2, 3);
+        tree.add(3, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        tree.add(6, 7);
+        assertThat(tree.add(7, 7)).isFalse();
+        assertThat(tree.add(0, 1)).isFalse();
+        assertThat(tree.add(8, 9)).isFalse();
+        assertThat(tree.add(7, 8)).isTrue();
+    }
+
+    @Test
+    void whenAddingSevenElementsInParallel() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(1, 5);
+        tree.add(1, 6);
+        tree.add(1, 7);
+        assertThat(tree.add(0, 1)).isFalse();
+        assertThat(tree.add(1, 7)).isFalse();
+        assertThat(tree.add(1, 8)).isTrue();
+    }
+}
