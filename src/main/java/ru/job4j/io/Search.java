bb41791a534f512cf752917ddd8739e0c09a00aa
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-07-17 14:19:25 +0400

    12. Валидация параметров запуска [#246865  [#246865]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,15 +8,36 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.function.Predicate;
 
+import static java.lang.String.format;
+
 public class Search {
-    public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    private static Boolean validateArgs(String arg0) {
+        File file = new File(arg0);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        return true;
     }
 
-    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+    private static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Invalid arguments.");
+        }
+        try {
+            validateArgs(args[0]);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Caught IOException: " + ex.getMessage());
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(format(".%s", args[1]))).forEach(System.out::println);
     }
 }
