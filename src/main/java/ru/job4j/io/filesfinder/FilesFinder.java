404ec6e72a4e318404408d91f47887f6144e04e9
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-08-11 21:51:50 +0400

    3. Поиск файлов по критерию [#783 #436004]

--- src/main/java/ru/job4j/io/filesfinder/FilesFinder.java
+++ src/main/java/ru/job4j/io/filesfinder/FilesFinder.java
@@ -10,6 +10,23 @@ import java.util.stream.Collectors;
 public class FilesFinder {
     private static ArgsName argsName;
 
+    private static String getRegexFromMask(String mask) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append('^');
+        mask.chars()
+                .forEach(ch -> {
+                    if (ch == '*') {
+                        stringBuilder.append(".*");
+                    } else if (ch == '?') {
+                        stringBuilder.append(".{1}");
+                    } else {
+                        stringBuilder.append(Character.toString(ch));
+                    }
+                });
+        stringBuilder.append('$');
+        return stringBuilder.toString();
+    }
+
     private static List<String> getFindedFiles(List<FileProperty> allFiles) {
         List<FileProperty> listOfFindedFileProperties = null;
         if ("name".equals(argsName.get("t"))) {
@@ -17,9 +34,15 @@ public class FilesFinder {
                     .filter(file -> file.getName().contains(argsName.get("n")))
                     .collect(Collectors.toList());
         }
+        if ("mask".equals(argsName.get("t"))) {
+            Pattern pattern = Pattern.compile(getRegexFromMask(argsName.get("n")));
+            listOfFindedFileProperties = allFiles.stream()
+                    .filter(file -> pattern.matcher(file.getName()).matches())
+                    .collect(Collectors.toList());
+        }
         if ("regex".equals(argsName.get("t"))) {
             Pattern pattern = Pattern.compile(argsName.get("n"));
-             listOfFindedFileProperties = allFiles.stream()
+            listOfFindedFileProperties = allFiles.stream()
                     .filter(file -> pattern.matcher(file.getName()).matches())
                     .collect(Collectors.toList());
         }
@@ -61,10 +84,38 @@ public class FilesFinder {
                 || argsName.get("o") == null) {
             throw new IllegalArgumentException("Invalid arguments");
         }
+        if (Files.notExists(Path.of(argsName.get("d")))) {
+            throw new IllegalArgumentException("Path not exists");
+        }
+        File result = new File(argsName.get("o"));
+        if (!result.exists()) {
+            try {
+                result.createNewFile();
+            } catch (IOException ioException) {
+                throw new IllegalArgumentException("Path not exists");
+            }
+        }
+        String mode = argsName.get("t");
+        if (!"name".equals(mode)
+                && !"mask".equals(mode)
+                && !"regex".equals(mode)) {
+            throw new IllegalArgumentException("Invalid search type");
+        }
         return argsName;
     }
 
+    private static void printHint() {
+        System.out.println("Программа должна запускаться с параметрами: ");
+        System.out.println("Ключи \n"
+                + "-d - директория, в которой начинать поиск.\n"
+                + "-n - имя файла, маска, либо регулярное выражение.\n"
+                + "-t - тип поиска: mask искать по маске, name по полному совпадение имени, regex по регулярному выражению.\n"
+                + "-o - результат записать в файл.\n");
+        System.out.println("Например:  -d=c:  -n=*.?xt -t=mask -o=log.txt");
+    }
+
     public static void main(String[] args) throws IOException {
+        printHint();
         argsName = getVerifiedParams(args);
         filesFind();
     }
