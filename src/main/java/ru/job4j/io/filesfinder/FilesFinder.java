27d967d254ad3c12a0ce9cdccaabb1c5a6dbc133
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-08-13 23:20:59 +0400

    3. Поиск файлов по критерию [#783 #436004]

--- src/main/java/ru/job4j/io/filesfinder/FilesFinder.java
+++ src/main/java/ru/job4j/io/filesfinder/FilesFinder.java
@@ -1,9 +1,9 @@
 package ru.job4j.io.filesfinder;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -11,48 +11,49 @@ public class FilesFinder {
     private static ArgsName argsName;
 
     private static String getRegexFromMask(String mask) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append('^');
-        mask.chars()
-                .forEach(ch -> {
-                    if (ch == '*') {
-                        stringBuilder.append(".*");
-                    } else if (ch == '?') {
-                        stringBuilder.append(".{1}");
-                    } else {
-                        stringBuilder.append(Character.toString(ch));
-                    }
-                });
-        stringBuilder.append('$');
-        return stringBuilder.toString();
+        mask = mask.replace(".", "[.]")
+                .replace("*", ".+")
+                .replace("?", ".");
+        mask = "^%s$".formatted(mask);
+        return mask;
     }
 
-    private static List<String> getFindedFiles(List<FileProperty> allFiles) {
-        List<FileProperty> listOfFindedFileProperties = null;
-        if ("name".equals(argsName.get("t"))) {
-            listOfFindedFileProperties = allFiles.stream()
-                    .filter(file -> file.getName().contains(argsName.get("n")))
-                    .collect(Collectors.toList());
-        }
+    private static void filesFind() throws IOException {
+        Predicate<Path> predicate = null;
+        List<Path> resultOfSearch = null;
         if ("mask".equals(argsName.get("t"))) {
             Pattern pattern = Pattern.compile(getRegexFromMask(argsName.get("n")));
-            listOfFindedFileProperties = allFiles.stream()
-                    .filter(file -> pattern.matcher(file.getName()).matches())
-                    .collect(Collectors.toList());
+            predicate = new Predicate<Path>() {
+                @Override
+                public boolean test(Path path) {
+                    return pattern.matcher(path.toFile().getName()).matches();
+                }
+            };
+        }
+        if ("name".equals(argsName.get("t"))) {
+            String name = argsName.get("n");
+            predicate = new Predicate<Path>() {
+                @Override
+                public boolean test(Path path) {
+                    return name.equals(path.toFile().getName());
+                }
+            };
         }
         if ("regex".equals(argsName.get("t"))) {
-            Pattern pattern = Pattern.compile(argsName.get("n"));
-            listOfFindedFileProperties = allFiles.stream()
-                    .filter(file -> pattern.matcher(file.getName()).matches())
-                    .collect(Collectors.toList());
+            Pattern pattern = Pattern.compile(getRegexFromMask(argsName.get("n")));
+            predicate = new Predicate<Path>() {
+                @Override
+                public boolean test(Path path) {
+                    return pattern.matcher(path.toFile().getName()).matches();
+                }
+            };
         }
-        List<String> findedFiles = null;
-        if (listOfFindedFileProperties != null && listOfFindedFileProperties.size() > 0) {
-            findedFiles = listOfFindedFileProperties.stream()
-                    .map(file -> file.toString())
-                    .collect(Collectors.toList());
+        resultOfSearch = Searcher.search(Path.of(argsName.get("d")), predicate);
+        if (resultOfSearch.size() > 0) {
+            saveDataToFile(resultOfSearch.stream()
+                    .map(path -> path.toString())
+                    .collect(Collectors.toList()));
         }
-        return findedFiles;
     }
 
     private static void saveDataToFile(List<String> resultOfFinding) {
@@ -67,33 +68,16 @@ public class FilesFinder {
         }
     }
 
-    private static void filesFind() throws IOException {
-        FindVisitor findVisitor = new FindVisitor();
-        Files.walkFileTree(Path.of(argsName.get("d")), findVisitor);
-        saveDataToFile(getFindedFiles(findVisitor.getAllFiles()));
-    }
-
     private static ArgsName getVerifiedParams(String[] args) {
         if (args.length != 4) {
             throw new IllegalArgumentException("Invalid number of arguments");
         }
-        ArgsName argsName = ArgsName.of(args);
+        argsName = ArgsName.of(args);
         if (argsName.get("d") == null
                 || argsName.get("n") == null
                 || argsName.get("t") == null
                 || argsName.get("o") == null) {
-            throw new IllegalArgumentException("Invalid arguments");
-        }
-        if (Files.notExists(Path.of(argsName.get("d")))) {
-            throw new IllegalArgumentException("Path not exists");
-        }
-        File result = new File(argsName.get("o"));
-        if (!result.exists()) {
-            try {
-                result.createNewFile();
-            } catch (IOException ioException) {
-                throw new IllegalArgumentException("Path not exists");
-            }
+            throw new IllegalArgumentException("Not all arguments are provided");
         }
         String mode = argsName.get("t");
         if (!"name".equals(mode)
--- src/main/java/ru/job4j/io/filesfinder/FindVisitor.java
+++ src/main/java/ru/job4j/io/filesfinder/FindVisitor.java
@@ -7,19 +7,26 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class FindVisitor extends SimpleFileVisitor<Path> {
-    private final List<FileProperty> allFiles = new ArrayList<>();
+    private Predicate<Path> condition;
 
-    public List<FileProperty> getAllFiles() {
-        return allFiles;
+    private List<Path> paths = new ArrayList<>();
+
+    public FindVisitor(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    public List<Path> getPaths() {
+        return paths;
     }
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        FileProperty currentFile = new FileProperty(attrs.size(),
-                file.getFileName().toString(), file.toAbsolutePath().toString());
-        allFiles.add(currentFile);
+        if (condition.test(file)) {
+            paths.add(file);
+        }
         return super.visitFile(file, attrs);
     }
 }
