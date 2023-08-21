0a12ee694a55da95a501077c9ddeaaf57a7fc974
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-08-21 18:00:27 +0400

    2. Схема прав пользователи и роли. [#1093 #438353]

--- db/2. user roles/create.sql
+++ db/2. user roles/create.sql
@@ -6,16 +6,11 @@ create table users (
 create table roles (
     id serial primary key,
     title varchar(255),
-    description text
+    description text,
+    user_id integer references users(id)
 );
 
-create table users_roles (
-    id serial primary key,
-    user_id integer references users(id),
-    role_id integer references roles(id)
-);
-
-create table rules_of_role (
+create table rules (
     id serial primary key,
     title varchar(255),
     description text
@@ -23,58 +18,40 @@ create table rules_of_role (
 
 create table roles_rules (
     id serial primary key,
-    roles_id integer references roles(id),
-    rules_id integer references rules_of_role(id)
+    role_id integer references roles(id),
+    rule_id integer references rules(id)
 );
 
-create table items (
-    id serial primary key,
-    title varchar(255),
-    description varchar(255)
-);
-
-create table states (
-    id serial primary key,
-    title varchar(255)
-);
-
-create table items_state (
-    id serial primary key,
-    item_id integer references items(id) unique,
-    state_id integer references states(id)
-);
 
 create table categories (
     id serial primary key,
     title varchar(255)
 );
 
-create table items_categories (
+create table items (
     id serial primary key,
-    item_id integer references items(id) unique,
+    title varchar(255),
+    description text,
+    user_id integer references users(id),
     categorie_id integer references categories(id)
+);
+
+create table states (
+    id serial primary key,
+    title varchar(255),
+    item_id integer references items(id)
 );
 
 create table comments (
     id serial primary key,
     title varchar(255),
-    description text
-);
-
-create table items_comments (
-    id serial primary key,
-    item_id integer references items(id),
-    comment_id integer references comments(id) unique
+    description text,
+    item_id integer references items(id)
 );
 
 create table attachs (
     id serial primary key,
     title varchar(255),
-    path_to_file text
-);
-
-create table items_attachs (
-    id serial primary key,
-    item_id integer references items(id),
-    attach_id integer references attachs(id) unique
+    path_to_file text,
+    item_id integer references items(id)
 );
--- db/2. user roles/insert.sql
+++ db/2. user roles/insert.sql
@@ -1,74 +1,59 @@
-insert into users(name_of_user) values ('Михаил');
-insert into users(name_of_user) values ('Анастасия');
-insert into users(name_of_user) values ('Елена');
-insert into users(name_of_user) values ('Марина');
+insert into users(id, name_of_user) values (1, 'Михаил');
+insert into users(id, name_of_user) values (2, 'Анастасия');
+insert into users(id, name_of_user) values (3, 'Елена');
+insert into users(id, name_of_user) values (4, 'Марина');
 
-insert into roles (title, description)
-    values ('Директор', 'Руководитель организации');
-insert into roles (title, description)
-    values ('Главный бухгалтер', 'Главный бухгалтер организации');
-insert into roles (title, description)
-    values ('Руководитель отдела кадров', 'Руководитель отдела кадров организации');
-insert into roles (title, description)
-    values ('Продавец-кассир', 'Продавец-кассир торгового зала');
+insert into roles (title, description, user_id)
+    values ('Инженер-прогаммист', 'Адиминистратор ИС', 1);
+insert into roles (title, description, user_id)
+    values ('Бухгалтер', 'Главный бухгалтер организации', 2);
+insert into roles (title, description, user_id)
+    values ('Руководитель отдела продаж', 'Руководитель отдела продаж организации', 3);
+insert into roles (title, description, user_id)
+    values ('Продавец-кассир', 'Продавец-кассир торгового зала', 4);
 
-insert into users_roles (user_id, role_id) values (1, 1);
-insert into users_roles (user_id, role_id) values (2, 2);
-insert into users_roles (user_id, role_id) values (3, 3);
-insert into users_roles (user_id, role_id) values (4, 4);
+insert into rules (id, title, description)
+    values (1, 'Администратор', 'Администратор ИС');
+insert into rules (id, title, description)
+    values (2, 'Оператор', 'Оператор ИС');
 
-insert into rules_of_role (title, description)
-    values ('Администратор', 'Администратор ИС');
-insert into rules_of_role (title, description)
-    values ('Оператор', 'Оператор ИС');
+insert into roles_rules (role_id, rule_id) values (1, 1);
+insert into roles_rules (role_id, rule_id) values (1, 2);
+insert into roles_rules (role_id, rule_id) values (2, 2);
+insert into roles_rules (role_id, rule_id) values (3, 2);
+insert into roles_rules (role_id, rule_id) values (4, 2);
 
-insert into roles_rules (roles_id, rules_id) values (1, 1);
-insert into roles_rules (roles_id, rules_id) values (1, 2);
-insert into roles_rules (roles_id, rules_id) values (2, 1);
-insert into roles_rules (roles_id, rules_id) values (2, 2);
-insert into roles_rules (roles_id, rules_id) values (3, 2);
-insert into roles_rules (roles_id, rules_id) values (4, 2);
+insert into categories (id, title) values (1, 'Услуга');
+insert into categories (id, title) values (2, 'Товар');
 
-insert into items (title, description)
-    values ('HQ77012', 'Подшипник ступицы задний');
-insert into items (title, description)
-    values ('CF-940C', 'Фильтр салонный угольный');
-insert into items (title, description)
-    values ('105_107', 'Регулятор холостого хода ОРИГИНАЛ');
+insert into items (id, title, description, user_id, categorie_id)
+    values (1, 'HQ77012', 'Подшипник ступицы задний', 4, 2);
+insert into items (id, title, description, user_id, categorie_id)
+    values (2, 'CF-940C', 'Фильтр салонный угольный', 4, 2);
+insert into items (id, title, description, user_id, categorie_id)
+    values (3, '105_107', 'Регулятор холостого хода ОРИГИНАЛ', 3, 2);
 
-insert into states (title) values ('в работе');
-insert into states (title) values ('выполнено');
+insert into states (id, title, item_id)
+    values (1, 'Заказ №1 - в работе', 1);
+insert into states (id, title, item_id)
+    values (2, 'Заказ №1 - выполнен', 1);
+insert into states (id, title, item_id)
+    values (3, 'Заказ №2 - в работе', 2);
+insert into states (id, title, item_id)
+    values (4, 'Заказ №2 - выполнен', 2);
+insert into states (id, title, item_id)
+    values (5, 'Заказ №3 - поставщик ожидает оплату', 3);
 
-insert into items_state (item_id, state_id) values (1, 1);
-insert into items_state (item_id, state_id) values (2, 1);
-insert into items_state (item_id, state_id) values (3, 1);
+insert into comments (title, description, item_id)
+    values ('Передать ООО "Авангард"', 'передать до 31.08.2023', 1);
+insert into comments (title, description, item_id)
+    values ('Для ООО "Вымпел"', 'представитель подъедет в офис 28.08.2023 до 12.00', 2);
+insert into comments (title, description, item_id)
+    values ('Товар отсутсвует на складе', 'Поставщик ожидает оплаты', 3);
 
-insert into categories (title) values ('услуги');
-insert into categories (title) values ('товары');
-
-insert into items_categories (item_id, categorie_id) values (1, 2);
-insert into items_categories (item_id, categorie_id) values (2, 2);
-insert into items_categories (item_id, categorie_id) values (3, 2);
-
-insert into comments (title, description)
-    values ('Передать ООО "Авангард"', 'передать до 31.08.2023');
-insert into comments (title, description)
-    values ('Для ООО "Вымпел"', 'представитель подъедет в офис 28.08.2023 до 12.00');
-insert into comments (title, description)
-    values ('Товар отсутсвует на складе', 'Поставщик ожидает оплаты');
-
-insert into items_comments (item_id, comment_id) values (1, 1);
-insert into items_comments (item_id, comment_id) values (2, 2);
-insert into items_comments (item_id, comment_id) values (3, 3);
-
-insert into attachs (title, path_to_file)
-    values ('Счёт-фактура №47457 от 21.08.2023', '/data/invoices/47457.invc');
-insert into attachs (title, path_to_file)
-    values ('Счёт-фактура №8759 от 11.07.2023', '/data/invoices/8759.invc');
-insert into attachs (title, path_to_file)
-    values ('Счёт в оплату №54155 от 14.08.2023', '/data/payment/54155.pay');
-
-insert into items_attachs (item_id, attach_id) values (1, 1);
-insert into items_attachs (item_id, attach_id) values (2, 2);
-insert into items_attachs (item_id, attach_id) values (3, 3);
-
+insert into attachs (title, path_to_file, item_id)
+    values ('Счёт-фактура №47457 от 21.08.2023', '/data/invoices/47457.invc', 1);
+insert into attachs (title, path_to_file, item_id)
+    values ('Счёт-фактура №8759 от 11.07.2023', '/data/invoices/8759.invc', 2);
+insert into attachs (title, path_to_file, item_id)
+    values ('Счёт в оплату №54155 от 14.08.2023', '/data/payment/54155.pay', 3);
