44f64e690240252a4fa8bba2e0a67fde27b057a4
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-09-10 15:15:44 +0400

    10. Операторы множества в SQL [#504896]

---
+++ db/10. union operators in SQL/exercise.sql
@@ -0,0 +1,37 @@
+create table movie (
+    id serial primary key,
+    name text,
+    director text
+);
+
+create table book (
+    id serial primary key,
+    title text,
+    author text
+);
+
+insert into movie (name, director)
+values ('Марсианин', 'Ридли Скотт'),
+       ('Матрица', 'Братья Вачовски'),
+       ('Властелин колец', 'Питер Джексон'),
+       ('Гарри Поттер и узник Азкабана', 'Альфонсо Куарон'),
+       ('Железный человек', 'Джон Фавро');
+
+insert into book (title, author)
+values ('Гарри Поттер и узник Азкабана', 'Джоан Роулинг'),
+       ('Властелин колец', 'Джон Толкин'),
+       ('1984', 'Джордж Оруэлл'),
+       ('Марсианин', 'Энди Уир'),
+       ('Божественная комедия', 'Данте Алигьери');
+
+select b.title as "фильмы, которые сняты по книге" from book b
+intersect
+select m.name  from movie m;
+
+select b.title as "книги, у которых нет экранизации" from book b
+except
+select m.name  from movie m;
+
+select b.title as "уникальные названия произведений" from book b
+union
+select m.name  from movie m;
