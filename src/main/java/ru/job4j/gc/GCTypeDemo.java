3a637672611a1dc500aeb0f872102653189c29d5
MikhailChrn <mikhail.cherneyev@yandex.ru>
2024-03-28 19:50:42 +0400

    1. Виды сборщиков мусора [#6852 #488615]

---
+++ src/main/java/ru/job4j/gc/GCTypeDemo.java
@@ -0,0 +1,35 @@
+package ru.job4j.gc;
+
+/**
+ * Запуск приложения с различными сборщиками мусора
+ * Для начала стоит отметить флаг -Xlog:gc*
+ * (до JDK 9 нужно использовать -XX:-PrintGCDetails)
+ * если мы его зададим то, сможем увидеть лог сборщика в консоли
+ *
+ * Ключи для запуска:
+ *
+ * - Serial => -XX:+UseSerialGC *
+ * - Parallel => -XX:+UseParallelGC *
+ * - CMS => -XX:+UseConcMarkSweepGC (доступен до JDK 14) *
+ * - G1 => -XX:+UseG1GC *
+ * - ZGC => -XX:+UseZGC
+ *
+ * Будем запускать такую программу.
+ * Она составляет строку из продублированного несколько раз символа,
+ * при этом перезаписывая ячейки массива.
+ */
+
+import java.util.Random;
+
+public class GCTypeDemo {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int length = 100;
+        String[] data = new String[1_000_000];
+        for (int i = 0; ; i = (i + 1) % data.length) {
+            data[i] = String.valueOf(
+                    (char) random.nextInt(255)
+            ).repeat(length);
+        }
+    }
+}
